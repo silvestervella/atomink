31edc721cd57d5009852e7968efcb2fe442d25cc
unknown <sylvester.vella@armanagement.net>
2018-08-20 17:57:21 +0200

    mela

--- app/wp-content/themes/html5blank-stable-child/template-single-games.php
+++ app/wp-content/themes/html5blank-stable-child/template-single-games.php
@@ -10,14 +10,15 @@ get_header();
 
   <main role="main" id="home">
     <div id="active-post"><img src="" alt="tattoo image" /></div>
-    <div class="page-title"><span><?php the_title(); ?></span></div>
-
-    <div id="controls">
-      <div id="prev-post">Prev</div>
-      <div id="next-post">Next</div>
-    </div>
 
     <div id="gallery-posts-outer">
+      <div class="page-title"><span><?php the_title(); ?></span></div>
+      <div id="controls">
+        <div id="prev-post">Prev</div>
+        <div id="next-post">Next</div>
+      </div>
+
+
       <?php atominktheme_generate_gallery_posts('images' , 'date' , 'ASC' , '' , '' , '' , 'gallery-images' ); ?>
     </div>
   </main>
