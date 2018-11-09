a5f728e1e70c97f78e1239a6f9dac238e5ab4365
unknown <sylvester.vella@armanagement.net>
2018-11-09 09:07:50 +0100

    redv

--- app/wp-content/themes/html5blank-stable-child/functions.php
+++ app/wp-content/themes/html5blank-stable-child/functions.php
@@ -25,6 +25,8 @@
  * 23. Get post thumbnail outside loop
  * 24. Products generator
  * 25. WooCommerce
+ * 26. Shop info menu item
+ * 27. Set Ink CPT title to post Id
  */
 
  
@@ -550,7 +552,33 @@ function atominktheme_generate_posts($atts) {
                 <div class="back-next"></div>
             </div>
             <div id="gallery-link">
-            <a href="<?php echo esc_url( get_permalink( get_post(212) ) ); ?>">VIEW<br/>ALL</a>
+            <a href="<?php echo esc_url( get_permalink( get_post(212) ) ); ?>">
+            
+            <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                viewBox="0 0 140.3 136.3" style="enable-background:new 0 0 140.3 136.3;" xml:space="preserve">
+            <style type="text/css">
+                .st0{fill:#DAD7CD;}
+            </style>
+            <g>
+                <g>
+                    <rect class="st0" width="41.7" height="41.7"/>
+                    <rect x="49.3" class="st0" width="41.7" height="41.7"/>
+                    <rect x="98.7" class="st0" width="41.7" height="41.7"/>
+                </g>
+                <g>
+                    <rect y="47.3" class="st0" width="41.7" height="41.7"/>
+                    <rect x="49.3" y="47.3" class="st0" width="41.7" height="41.7"/>
+                    <rect x="98.7" y="47.3" class="st0" width="41.7" height="41.7"/>
+                </g>
+                <g>
+                    <rect y="94.7" class="st0" width="41.7" height="41.7"/>
+                    <rect x="49.3" y="94.7" class="st0" width="41.7" height="41.7"/>
+                    <rect x="98.7" y="94.7" class="st0" width="41.7" height="41.7"/>
+                </g>
+            </g>
+            </svg>
+
+            VIEW<br/>ALL</a>
             </div>
             <div id="active-post">
                 <div id="img-outer">
@@ -853,7 +881,9 @@ function atominktheme_generate_posts($atts) {
 
 
 
-
+            /**
+             * 27. Set Ink CPT title to post Id
+             */
             function atominktheme_default_title( $post_title, $post ) {
                 if( 'images' == $post->post_type ) {
                     return $post->ID;
